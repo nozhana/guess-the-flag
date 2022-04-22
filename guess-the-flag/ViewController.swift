01eeed1af6c46cdd0d6d7d973af0d8ac6e9a9b51
Nozhan Amiri <nozhana@gmail.com>
2022-04-22 20:55:56 +0430

    Add scoring system Implement alert controller to display score Implement @IBAction to get button tap

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswerIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,14 +54,14 @@ class ViewController: UIViewController {
 //        button2.setImage(UIImage(named: choices[1]), for: .normal)
 //        button3.setImage(UIImage(named: choices[2]), for: .normal)
         
-        askQuestion()
+        askQuestion(action: nil)
 
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
         
         let choices = countries.shuffled()[0...2]
-        let correctAnswerIndex = Int.random(in: 0...2)
+        correctAnswerIndex = Int.random(in: 0...2)
         print(correctAnswerIndex)
         
         print(choices) // MARK: Check correct answers
@@ -72,7 +73,26 @@ class ViewController: UIViewController {
         title = "Pick the correct flag for \(choices[correctAnswerIndex].uppercased())"
         
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswerIndex {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Incorrect!"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+    }
+    
 
 }
 
